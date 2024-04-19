fefe4773200b7b9e8b1bcd8c6ebe63cfb58669dc
swapnil <swapnildas953@gmail.com>
2024-04-19 22:31:34 +0530

    commit

---
+++ DFS.cpp
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#define MAX 50
+
+int adjacencyM[MAX][MAX];
+int visited[MAX];
+int n;
+int stack[MAX];
+int top=-1;
+
+void push(int a){
+    stack[++top]=a;
+    return;
+}
+int pop(){
+    int item=stack[top];
+    top--;
+    return item;
+}
+
+void dfs(int n,int start_node){
+	int current_node;
+	push(start_node);
+	visited[start_node]=1;
+	while(top>=0){
+		current_node=pop();
+        visited[current_node]=1;
+		printf("%d -> ",current_node);
+		for(int i=0;i<n;i++){
+			if(adjacencyM[current_node][i] && !visited[i]){
+				push(i);
+			}
+		}
+	}
+	printf("end");
+}
+
+int main(){
+    FILE *fp=fopen("ip.txt","r");
+	fscanf(fp,"%d",&n);
+	printf("%d \n",n);
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			fscanf(fp,"%d",&adjacencyM[i][j]);
+		}
+	}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			printf("%d ",adjacencyM[i][j]);
+		}
+		printf("\n");
+	}
+	int start_node;
+	printf("enter the starting node : ");
+	scanf("%d",&start_node);
+	printf("The BFS traversal is ");
+	dfs(n,start_node);
+}
